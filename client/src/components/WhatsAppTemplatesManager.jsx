8dae8bfbe9ed67159cfe2297cc3f23eb7af5f3f7
Pau Adgorithm <pauj2015gs@gmail.com>
2025-12-06 18:34:15 +0100

    ddsd

--- client/src/components/WhatsAppTemplatesManager.jsx
+++ client/src/components/WhatsAppTemplatesManager.jsx
@@ -14,7 +14,7 @@ import {
 } from 'lucide-react';
 
 const WhatsAppTemplatesManager = () => {
-  const API_URL_BASE = 'https://chatgorithm-front-end.onrender.com/api';
+  const API_URL_BASE = 'https://chatgorithm.onrender.com/api';
 
   // Estado de plantillas (eliminado el tipo <any[]>)
   const [templates, setTemplates] = useState([]);
